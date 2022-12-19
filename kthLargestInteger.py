a29555e75b7d9cd71e5aed6f4a4494003447adea
addis <addisumotora3@gmail.com>
2022-12-19 14:41:05 +0300

    kth largest integer

---
+++ kthLargestInteger.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        
+        for i in range(len(nums)):
+            nums[i] = int(nums[i])
+        
+        nums.sort()
+        
+        return str(nums[-k])
