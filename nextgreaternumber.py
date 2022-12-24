7db58981c72157757944d422185356f2cbe0c563
addis <addisumotora3@gmail.com>
2022-12-24 23:16:23 +0300

    better one

--- nextgreaternumber.py
+++ nextgreaternumber.py
@@ -1,16 +1,15 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans = []
+
+        stack = []
         for i in nums1:
-            isg = False
-            isgg = False
-            for j in nums2:
-                if j == i:
-                    isg = True
-                if isg and j>i:
-                    ans.append(j)
-                    isgg = True
-                    break
-            if not isgg:
-                ans.append(-1)
-        return ans
+            flag=False
+            index = nums2.index(i)
+            for j in nums2[index:]:
+                if j>i:
+                    stack.append(j)
+                    flag=True
+                    break        
+            if not flag:
+                stack.append(-1)
+        return stack
