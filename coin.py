aabc43c5d7228897ce12285d32b1cccdcc4801ba
addis <addisumotora3@gmail.com>
2023-01-05 21:43:51 +0300

    coin

--- coin.py
+++ coin.py
@@ -12,3 +12,18 @@ class Solution:
             summ = summ + i
 
         return summ
+# better soln
+
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        l, r = 0, len(piles)
+        piles.sort()
+        summ = 0
+        stack = []
+        while l < r:
+            r-=2
+            stack.append(piles[r])
+            summ += piles[r]
+            l+=1
+        
+        return summ
