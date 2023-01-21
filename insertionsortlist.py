f9a0ec5cc6830d652c1782ae906a389a12c3bce0
addis <addisumotora3@gmail.com>
2023-01-21 15:43:14 +0300

    ....

--- insertionsortlist.py
+++ insertionsortlist.py
@@ -1,5 +1,32 @@
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+                dummyhead = ListNode(0,head)
+        prev,curr = head, head.next
+
+        while curr:
+
+            if curr.val >= prev.val:
+                prev, curr = curr, curr.next
+                continue
+            
+            temp = dummyhead
+
+            while curr.val > temp.next.val:
+                temp = temp.next
+
+            prev.next = curr.next
+            curr.next = temp.next
+            temp.next = curr
+            curr = prev.next
+
+        return dummyhead.next
+
+
+
+# /////////////////////////////
+
+
         stack = []
         current = head
 
