cc21ee6ccef7cf3e7a02fd61e195e92f13bae3bf
Addisu Motora <88212776+addisumotora@users.noreply.github.com>
2024-12-18 11:01:39 +0300

    Time: 0 ms (100%), Space: 18 MB (7.47%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -3,15 +3,27 @@ class Solution:
         hashMap = { val:i for i, val in enumerate(nums1)}
         result = [-1] * len(nums1)
 
-        for i in range(len(nums2)):
-            if nums2[i] not in hashMap:
-                continue
+        stack = []
 
-            for j in range(i + 1, len(nums2)):
-                if nums2[j] > nums2[i]:
-                    indx = hashMap[nums2[i]]
-                    result[indx] = nums2[j]
-                    break
+        for i in range(len(nums2)):
+            while stack and stack[-1] < nums2[i]:
+                index = hashMap[stack.pop()]
+                result[index] = nums2[i]
+
+            if nums2[i] in hashMap:
+                stack.append(nums2[i])
+
+        return result
+        
+        # for i in range(len(nums2)):
+        #     if nums2[i] not in hashMap:
+        #         continue
+
+        #     for j in range(i + 1, len(nums2)):
+        #         if nums2[j] > nums2[i]:
+        #             indx = hashMap[nums2[i]]
+        #             result[indx] = nums2[j]
+        #             break
 
         return result
 
