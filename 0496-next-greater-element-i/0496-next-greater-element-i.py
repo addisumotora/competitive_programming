ab279c0fa850014575738bdc2cb8c6d6ba21a856
Addisu Motora <88212776+addisumotora@users.noreply.github.com>
2024-12-18 10:48:51 +0300

    Time: 0 ms (100%), Space: 17.9 MB (7.47%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,18 +1,18 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        numsHashmap = {n:i for i,n in enumerate(nums1) }
-        output = [-1] * len(nums1)
-        stack = []
-        
+        hashMap = { val:i for i, val in enumerate(nums1)}
+        result = [-1] * len(nums1)
+
         for i in range(len(nums2)):
-            while stack and nums2[i] > stack[-1]:
-                val = stack.pop();
-                indx = numsHashmap[val]
-                output[indx] = nums2[i]
-            if nums2[i] in nums1:
-                stack.append(nums2[i])
-        return output
-        
-        
-        
+            if nums2[i] not in hashMap:
+                continue
+
+            for j in range(i + 1, len(nums2)):
+                if nums2[j] > nums2[i]:
+                    indx = hashMap[nums2[i]]
+                    result[indx] = nums2[j]
+                    break
+
+        return result
+
         
