f5cd0e25f2ee53e9e81e9c1b532b0a010148f87b
Addisu Motora <88212776+addisumotora@users.noreply.github.com>
2025-04-02 17:19:16 +0300

    Time: 4 ms (9.45%), Space: 18 MB (25.16%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -8,5 +8,7 @@ class Solution:
         for r in range(len(t)):
             if l < len(s) and s[l] == t[r]:
                 l += 1
-
+            elif l == len(s):
+                return True
+                
         return True if len(s) == l else False
